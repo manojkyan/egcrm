e53459a2944551d11e321e1984575b4b92c9f1a6
manojkyan <40230466+manojkyan@users.noreply.github.com>
2025-08-14 15:01:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/egcrm/', // 👈 This tells Vite your site is in a subfolder
+  base: './', // ✅ important for Netlify static uploads
 })
-
